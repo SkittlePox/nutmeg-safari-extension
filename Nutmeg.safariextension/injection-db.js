f5a52e488fa786dfd18c14bc520c4d04a7d4fe7a
SkittlePox <bspiegel11@gmail.com>
2016-08-17 20:19:39 -0400

    IndexedDB Update

---
+++ Nutmeg.safariextension/injection-db.js
@@ -0,0 +1,20 @@
+var NavNode = function(objStore, parentTitle, parentURL, childURL) {
+    this.objStore = objStore;
+    this.parentTitle = parentTitle;
+    this.parentURL = parentURL;
+    this.childURL = childURL;
+    this.childTitle = "";
+    // Child Title cannot be fetched at this point
+}
+
+function wasClicked(element) {
+    safari.self.tab.dispatchMessage("newNode", new NavNode("all-browsing", top.document.title, top.document.URL, element.href)); // If not null send new Node object
+}
+
+links = document.links;
+
+for (var i = 0; i < links.length; i++) {    // Injects function into each link
+    if (links[i].href && links[i].href != top.document.URL) {
+        links[i].addEventListener("click", function() {wasClicked(this);});
+    }
+}
